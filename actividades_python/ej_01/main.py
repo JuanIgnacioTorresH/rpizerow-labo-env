afa0c8ff88d77759b3744f2a3b9d67fbc64350fe
JuanIgnacioTorresH <juaningaciotorr@gmail.com>
2024-03-27 20:17:21 +0000

    ejercicio-1

--- actividades_python/ej_01/main.py
+++ actividades_python/ej_01/main.py
@@ -1,26 +1,14 @@
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED, Button
+from signal import pause
 #Importo librerias necesarias
 
 button = Button (18)
-#Asigno el pin correspondiente al botón a una variable
+#Asigno el pin del botón a una variable
 ledR = LED (19)
-ledG = LED (13)
-ledB = LED (26)
-#Asigno los pines de los led a variables (R para rojo, G para verde, B para azul)
+#Asigno al pin de uno de los led (el rojo) a una variables
 
-while (True):
-    ledR.on
-    sleep (0.5)
-    ledR.off
-    sleep (0.5)
-    #Enciendo el LED Rojo por medio segundo y lo apago
-    ledG.on
-    sleep (0.5)
-    ledG.off
-    sleep (0.5)
-    #Enciendo el LED Verde por medio segundo y lo apago
-    ledB.on
-    sleep (0.5)
-    ledB.off
-    #Enciendo el LED Azul por medio segundo y lo apago
+button.when_pressed = ledR.on
+#Propiedad que cuando el botón esta apretado, se prende el led
+button.when_released = ledR.off
+#Propiedad que cuando el botón se suelta o no está apretado, se apaga
+pause()
